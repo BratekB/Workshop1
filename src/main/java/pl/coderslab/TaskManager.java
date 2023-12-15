13397e31bab28f5a79ecc135ef4a8e4e4b036531
BratekB <bebe77@gazeta.pl>
2023-12-15 23:51:17 +0100

    printMenu

--- src/main/java/pl/coderslab/TaskManager.java
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -1,13 +1,52 @@
 package pl.coderslab;
 
+import java.util.Scanner;
+
 public class TaskManager {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        printMenu();
+        Scanner scanner = new Scanner(System.in);
+        while (!scanner.hasNext("exit")) {
+            switch (scanner.next()) {
+                case "add":
+                    addTask();
+                    break;
+                case "remove":
+                    removeTask();
+                    break;
+                case "list":
+                    listTask();
+                    break;
+                default:
+                    break;
+            }
+            printMenu();
+        }
+        System.out.println("End of program");
     }
 
-    public static void showMenu(){
-
+    public static void printMenu() {
+        System.out.println(ConsoleColors.GREEN + "Please select an option:"+ ConsoleColors.RESET);
+        System.out.println("add");
+        System.out.println("remove");
+        System.out.println("list");
+        System.out.println(ConsoleColors.BLUE + "exit" + ConsoleColors.RESET);
+        System.out.print("Your choice: ");
     }
 
+    public static void readTaskFile() {
+        System.out.printf(ConsoleColors.RED + "readTaskFile" + ConsoleColors.RESET);
+    }
 
+    public static void addTask() {
+        System.out.println(ConsoleColors.RED + "addTask" + ConsoleColors.RESET);
+    }
+
+    public static void listTask() {
+        System.out.println(ConsoleColors.RED + "listTask" + ConsoleColors.RESET);
+    }
+
+    public static void removeTask() {
+        System.out.println(ConsoleColors.RED + "removeTask" + ConsoleColors.RESET);
+    }
 }
